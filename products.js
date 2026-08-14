21d9587192592c685fb925a79e01ab207d56dd19
diabyh599-gif <diabyh599@gmail.com>
2026-08-14 21:00:41 +0000

    Create products.js

---
+++ products.js
@@ -0,0 +1,139 @@
+const savedProducts =
+JSON.parse(
+localStorage.getItem("products")
+);
+
+const products =
+savedProducts || [
+
+{
+id: 1,
+name: "Ensemble Élégant Premium",
+price: 7500,
+oldPrice: 9500,
+stock: 5,
+sales: 0,
+category: "Vêtements",
+event: "fête",
+rating: 4.8,
+reviews: 124,
+isNew: true,
+isBestSeller: true,
+comments: [
+"⭐⭐⭐⭐⭐ Très bonne qualité",
+"⭐⭐⭐⭐ Élégant et confortable",
+"⭐⭐⭐⭐⭐ Je recommande"
+],
+delivery: "🚚 Livraison 2 à 3 jours",
+images: [
+"https://i.ibb.co/0ykTzmM2/IMG-4374.jpg",
+"https://picsum.photos/600/600?11",
+"https://picsum.photos/600/600?12"
+]
+},
+
+{
+id: 2,
+name: "Nike Air Force One Blanche",
+price: 15000,
+oldPrice: 18000,
+stock: 3,
+sales: 0,
+category: "Chaussures",
+event: "sortie",
+rating: 4.9,
+reviews: 89,
+isNew: false,
+isBestSeller: true,
+comments: [
+"⭐⭐⭐⭐⭐ Très bonne qualité",
+"⭐⭐⭐⭐ Élégant et confortable",
+"⭐⭐⭐⭐⭐ Je recommande"
+],
+delivery: "🚚 Livraison 2 à 3 jours",
+images: [
+"https://i.ibb.co/5WVcQyYX/IMG-4378.jpg",
+"https://picsum.photos/600/600?21",
+"https://picsum.photos/600/600?22"
+]
+},
+
+{
+id: 3,
+name: "Samsung Galaxy Buds 3 Pro",
+price: 25000,
+oldPrice: 30000,
+stock: 4,
+sales: 0,
+category: "Électronique",
+event: "quotidien",
+rating: 4.7,
+reviews: 256,
+isNew: true,
+isBestSeller: false,
+comments: [
+"⭐⭐⭐⭐⭐ Très confortable",
+"⭐⭐⭐⭐ Livraison rapide",
+"⭐⭐⭐⭐⭐ Produit conforme"
+],
+delivery: "🚚 Livraison 2 à 3 jours",
+images: [
+"https://i.ibb.co/M4gzDNK/IMG-4379.jpg",
+"https://picsum.photos/600/600?31",
+"https://picsum.photos/600/600?32"
+]
+},
+
+{
+id: 4,
+name: "Montre POEDAGAR 885",
+price: 6500,
+oldPrice: 8500,
+stock: 6,
+sales: 0,
+category: "Accessoires",
+event: "fête",
+rating: 4.6,
+reviews: 178,
+isNew: false,
+isBestSeller: true,
+comments: [
+"⭐⭐⭐⭐⭐ Très confortable",
+"⭐⭐⭐⭐ Livraison rapide",
+"⭐⭐⭐⭐⭐ Produit conforme"
+],
+delivery: "🚚 Livraison demain",
+images: [
+"https://i.ibb.co/8n9Bxb4C/IMG-4376.jpg",
+"https://picsum.photos/600/600?41",
+"https://picsum.photos/600/600?42"
+]
+},
+
+{
+id: 5,
+name: "Sac pour Femme",
+price: 12000,
+oldPrice: 15000,
+stock: 4,
+sales: 0,
+category: "Accessoires",
+event: "sortie",
+rating: 4.8,
+reviews: 92,
+isNew: true,
+isBestSeller: false,
+comments: [
+"⭐⭐⭐⭐⭐ Très bonne qualité",
+"⭐⭐⭐⭐ Élégant et confortable",
+"⭐⭐⭐⭐⭐ Je recommande"
+],
+delivery: "🚚 Livraison 2 à 3 jours",
+images: [
+"https://i.ibb.co/MydF07fD/IMG-4377.jpg",
+"https://picsum.photos/600/600?51",
+"https://picsum.photos/600/600?52"
+]
+}
+
+];
